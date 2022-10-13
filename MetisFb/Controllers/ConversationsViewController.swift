c1886dc2da99c69f70bdcc5577e9c0cb98962bcf
“TalekC-137” <pawatl2002@gmail.com>
2022-10-13 22:08:45 +0200

    Initial Commit

---
+++ MetisFb/Controllers/ConversationsViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MetisFb
+//
+//  Created by Mac m1 on 13/10/2022.
+//
+
+import UIKit
+
+class ConversationsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
